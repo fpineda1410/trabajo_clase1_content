0d385b366b26adbc560c14c9b6a364f8dd0dc28d
fpineda1410 <fpineda147@outlook.com>
2022-04-25 20:14:56 -0600

    Practica uno Lista

---
+++ uno.js
@@ -0,0 +1,64 @@
+console.log('aqui estoy')
+
+/* ------------------------------------------------------------------------------------------
+1. var  y let, el let solo existe dentro del context (es decir un if, una funcion)
+
+var numero = 6;
+
+
+if (numero>5) {
+    let existe = 4;
+}
+
+console.log (existe);
+
+*/
+//-------------------------------------------------------------------------------------------
+//2. operaciones basicas
+
+// var papayas = 4;
+// var limones = 4;
+// var sandias = 2;
+
+// function suma_de_frutas (fruta_1,fruta_2){
+//     var suma_de_frutas = fruta_1 + fruta_2; //suma matematica
+//     console.log ('La cantidad de frutas es: '+suma_de_frutas); //concatenacion (suma de letras y numeros para escribir algo)
+// }
+
+// suma_de_frutas(sandias,limones)
+
+//*Trabajo en clase--> hacer resta de frutas
+
+
+//------------------------------------------------------------------------------
+//3.5 Hay partido de la sele 
+//hay entradas?
+//es de pie?
+// var hay_entradas = false;
+// var es_de_pie = true;
+
+
+// if (hay_entradas == true){
+
+//     if (es_de_pie==false){
+//         console.log('que salvada ver a navas sentado!!')
+//     }else{
+//         console.log('bueno por lo menos agarramos campo')
+//     }
+// }else{
+//     console.log('que madre, toco verlo por el 7')
+// }
+
+//*trabajo en clase --> 
+//*1. prguntar si me invitaron a la fiesta
+//*2.si hay queque
+if (invitaron_fiesta == true) {
+    if (hay_queque == true) {
+
+    }else{
+
+    }
+
+}else{
+
+}
